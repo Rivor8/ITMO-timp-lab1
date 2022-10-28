18a122144e4a7949c2dddd52f4278d15cd91609a
Aleksandr Vorobev <rivor08@gmail.com>
2022-10-28 17:02:50 -0400

    init

---
+++ task1/lab.c
@@ -0,0 +1,14 @@
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+    if (argc < 2) {
+        printf("Use 'seq_lab --help' to display command line options.");
+        return 0;
+    }
+
+
+
+    return 0;
+}
+
