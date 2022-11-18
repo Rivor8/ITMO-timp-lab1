90771a19186cf12ddbca096fda057f002ed38816
Aleksandr Vorobev <rivor08@gmail.com>
2022-11-18 11:30:19 +0300

    Added some comments

--- task2/script.js
+++ task2/script.js
@@ -3,20 +3,21 @@ let current_password = "";
 
 let pass_val = document.getElementById("pass");
 
+// Проверка введенного пароля
 let unlock = () => {
     current_password = MD5(pass_val.value);
     if (correct_password == current_password) alert("Контент разблокирован");
     else alert("Контент не разблокирован");
 }
 
-document.onselectstart = () => { return correct_password == current_password };
-document.ondragstart = () => { return correct_password == current_password };
-document.oncontextmenu = () => { return correct_password == current_password };
-document.oncopy = () => { return correct_password == current_password };
+document.onselectstart = () => { return correct_password == current_password }; // Запрет или разрешение выделения текста
+document.ondragstart = () => { return correct_password == current_password }; // Запрет или разрешение перемешения выделенного текста или картинок
+document.oncontextmenu = () => { return correct_password == current_password }; // Запрет или разрешение контекстного меню
+document.oncopy = () => { return correct_password == current_password }; // Запрет или разрешение копирования
 
 
 
-document.addEventListener('keyup', (e) => {
+document.addEventListener('keyup', (e) => { // При нажатии на PrintScreen исчезает содержимое и очищается буфер обмена
     if (e.key === 'PrintScreen') {
         if (correct_password != current_password) {
             document.body.hidden = true;
